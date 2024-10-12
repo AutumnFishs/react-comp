4e57f96c7e96e403eda99108e7e876aecc283010
何英杰 <305664823@qq.com>
2024-10-12 10:42:31 +0800

    fix:配置base

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+// process.env.GITHUB_REPOSITORY 存储仓库的全名  GitHub Actions 自动设置的标准环境变量之一
+const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY || ""
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: GITHUB_REPOSITORY ? `/${GITHUB_REPOSITORY}/` : "/", //在git上部署时，需要设置base，在vercel上部署时设置为/即可,
   build: {
     outDir: "dist",
     assetsDir: "static",
